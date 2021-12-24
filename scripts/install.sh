8c3488a8d197aff6515e8d0b5d3dd6d51c9b78c8
Alistair Scott <ali.scott@gmail.com>
2021-12-24 07:43:21 +0000

    enhance: add semver support to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env sh
-# This script is used in the README and https://www.infracost.io/docs/#quick-start
 set -e
+
+version_name=${INFRACOST_VERSION:-latest}
 
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | tr '[:upper:]' '[:lower:]' | sed -e s/x86_64/amd64/)
-echo "Downloading latest release of infracost-$os-$arch..."
-curl -sL https://github.com/infracost/infracost/releases/latest/download/infracost-$os-$arch.tar.gz | tar xz -C /tmp
-echo
-echo "Moving /tmp/infracost-$os-$arch to /usr/local/bin/infracost (you might be asked for your password due to sudo)"
-sudo mv /tmp/infracost-$os-$arch /usr/local/bin/infracost
-echo
+
+version=$version_name
+url="https://github.com/infracost/infracost/releases/latest/download/infracost-$os-$arch.tar.gz"
+if [ "$version_name" != "latest" ]; then
+  # TODO: add pagination support
+  resp=$(curl -sL https://api.github.com/repos/infracost/infracost/releases\?per_page\=100)
+  versions=$(echo "$resp" | sed -n 's/.*\"tag_name\": \"\(.*\)\".*/\1/p')
+
+  # If v0.x.y was passed in find the exact version
+  version=$(echo "$versions" | grep "$version_name$" | head -n 1)
+
+  # If v0.x was passed in find the latest v0.x version
+  if [ -z "$version" ]; then
+    version=$(echo "$versions" | grep "$version_name." | head -n 1)
+  fi
+
+  url="https://github.com/infracost/infracost/releases/download/$version/infracost-$os-$arch.tar.gz"
+fi
+
+echo "Downloading $version release of infracost-$os-$arch..."
+curl -sL $url | tar xz -C /tmp
+
+echo "Moving /tmp/infracost-$os-$arch to /usr/local/bin/infracost"
+
+mv -f /tmp/infracost-$os-$arch /usr/local/bin/infracost
+
 echo "Completed installing $(infracost --version)"
